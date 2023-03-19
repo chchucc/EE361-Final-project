891f96b4f63009c91b29122dfae007b1fea018a4
ahmadfantastic <ahmadfantastic.ads@gmail.com>
2023-03-19 07:37:27 -0400

    Fix database connection issue due late start of mysql server. Solved by adding a 30 sec delay

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python manage.py makemigrations
-python manage.py makemigrations keys
+echo "Sleeping for 30 sec to allow database server to start"
+sleep 30
+echo "Setting up and Running the Application"
 python manage.py migrate
 python manage.py runserver "0.0.0.0:8000"
