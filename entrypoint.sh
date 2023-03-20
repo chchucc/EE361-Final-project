b6ec21a12c908b55b0368c6c7712552ec4d96b6f
ahmadfantastic <ahmadfantastic.ads@gmail.com>
2023-03-20 13:40:11 -0400

    - Added database dependency condition with healthcheck - Remove sleep delay in entrypoint - Added default database password

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "Sleeping for 30 sec to allow database server to start"
-sleep 30
 echo "Setting up and Running the Application"
 python manage.py migrate
 python manage.py runserver "0.0.0.0:8000"
