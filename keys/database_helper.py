ea0266892914544521563d86f1c6dee92134479c
ahmadfantastic <ahmadfantastic.ads@gmail.com>
2023-03-21 22:19:52 -0400

    - fix states table name error

--- keys/database_helper.py
+++ keys/database_helper.py
@@ -2,7 +2,7 @@ from django.db import connection
 
 def get_all_states():
     with connection.cursor() as cursor:
-        cursor.execute("SELECT  * FROM state")
+        cursor.execute("SELECT  * FROM states")
         columns = [col[0] for col in cursor.description]
         return [
             dict(zip(columns, row))
